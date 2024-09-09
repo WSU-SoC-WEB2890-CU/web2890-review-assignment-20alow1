020ab847733519eb9ae0fc2b6660c383d45438ea
Avrey Low <avreylow@gmail.com>
2024-09-08 18:21:26 -0600

    Final submission for review

--- src/javascripts/main.js
+++ src/javascripts/main.js
@@ -1,2 +1,8 @@
 //TODO - Your ES6 JavaScript code (if any) goes here
 import 'bootstrap'
+
+//I can't get it to work... js doesn't think bootstrap is imported
+
+// var scrollSpy = new bootstrap.ScrollSpy(document.body, {
+//     target: '#navbar'
+//   })
